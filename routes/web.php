f74b01a9e46d4d73e3d159815af12752a2c66f79
matheusAparicio <matheusilva-m@outlook.com>
2025-11-25 02:48:25 -0300

    turnaround pro shell do railway

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/rodar-migrations-secreta', function () {
+    // Roda o comando forçando (pois é produção)
+    Artisan::call('migrate:fresh --seed --force');
+    
+    return "Banco resetado e populado com sucesso! <br>" . nl2br(Artisan::output());
+});
